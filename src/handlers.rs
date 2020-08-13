f820d08f3d2c96ae24f8952a10dab093a39ea23f
Ian Holsman <ian@holsman.org>
2020-08-13 12:45:12 -0400

    update home page a bit. still TODO: form processing

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,4 @@
-use actix_web::web::Query;
+use actix_web::web::{Query, Json};
 use actix_web::{web, HttpResponse, Responder};
 
 use eosio_client_api::json_rpc::{AbiTrio, EOSRPC};
@@ -7,6 +7,25 @@ use std::collections::HashMap;
 use eosio_client_api::api_types::AbiStruct;
 
 pub async fn index() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(
+        "<h1>Account Explorer</h1><h2>System</h2><ul>".to_owned()
+            + "<li><a href='/account/eosio'>eosio</a></li>"
+            + "<li><a href='/account/eosio.token'>eosio.token</a></li>"
+
+            + "</ul><h2>Regular</h2><ul>"
+            +"<li><a href='/account/fwonhjnefmps'>fwonhjnefmps(Tic Tac Toe)</a></li>"
+            + "<li><a href='/account/exoejrppvyew'>exoejrppvyew</a></li>"
+            + "<li><a href='/account/wmftggomuzsi'>wmftggomuzsi</a></li>"
+            + "</ul>"
+        +"<h2>Resource Market</h2><ul>"
+            +"<li><a href='/account/eosio/rows/eosio.reserv/userres'>eosio.reserv - userres</a></li>"
+            +"<li><a href='/account/eosio/rows/eosio/rammarket'>eosio - ram market</a></li>"
+        +"</ul>",
+    )
+}
+pub async fn action_detail_post(info: web::Path<(String,String)>/*,params: Json<HashMap<String,String>>*/) -> impl Responder {
+    println!("{:?}",info);
+   // println!("{:?}",params);
     HttpResponse::Ok().content_type("text/html").body(
         "<h1>Account Explorer</h1><ul><li><a href='/account/eosio'>eosio</a></li>".to_owned()
             + "<li><a href='/account/fwonhjnefmps'>fwonhjnefmps(Tic Tac Toe)</a></li>"
@@ -17,7 +36,7 @@ pub async fn index() -> impl Responder {
 }
 
 pub async fn account_detail(info: web::Path<String>) -> impl Responder {
-    let host = "https://api.testnet.eos.io";
+    let host = "http://tempest.local:8888";
     let acc = String::from(info.as_str());
     match EOSRPC::non_blocking(String::from(host)).await {
         Ok(eos) => match eos.get_abi(&acc).await {
@@ -50,7 +69,7 @@ pub async fn account_detail(info: web::Path<String>) -> impl Responder {
                         .iter()
                         .map(|type_def| {
                             format!(
-                               "<li>{} {}</li>", type_def.abi_type, type_def.new_type_name
+                                "<li>{} {}</li>", type_def.abi_type, type_def.new_type_name
                             )
                         })
                         .collect::<Vec<_>>();
@@ -78,7 +97,7 @@ pub async fn account_detail(info: web::Path<String>) -> impl Responder {
 }
 
 pub async fn table_detail(params: web::Path<(String, String)>) -> impl Responder {
-    let host = "https://api.testnet.eos.io";
+    let host = "http://tempest.local:8888";
     let acc = String::from(params.0.as_str());
     let table_name = String::from(params.1.as_str());
     match EOSRPC::non_blocking(String::from(host)).await {
@@ -107,43 +126,115 @@ pub async fn table_detail(params: web::Path<(String, String)>) -> impl Responder
         Err(e) => HttpResponse::InternalServerError().body(String::from(e.description())),
     }
 }
-fn dump_struct(structs:&HashMap<String, AbiStruct>, name:&str) -> String {
-    dump_struct_int(&structs, name, 0)
+
+fn dump_struct(structs: &HashMap<String, AbiStruct>, name: &str, as_form: bool) -> String {
+    dump_struct_int(&structs, name, "", as_form, 0)
 }
-fn dump_struct_int(structs:&HashMap<String, AbiStruct>, name:&str, level:usize) -> String {
+
+fn dump_struct_int(structs: &HashMap<String, AbiStruct>, name: &str, prefix: &str, as_form: bool, level: usize) -> String {
     if level > 5 {
-        return "".to_owned()
+        return "".to_owned();
     } else {
         match structs.get(name) {
             Some(st) => {
                 let mut resp = String::new();
                 resp.push_str(name);
-                resp.push_str( "{");
+                resp.push_str("<ul>");
                 if !st.base.is_empty() {
-                    resp.push_str("base:{");
-                    resp.push_str(&dump_struct_int(structs,&st.base,level+1));
-                    resp.push_str("}");
+                    resp.push_str(&dump_struct_int(structs, &st.base, &format!("{}{}.base.", prefix, name), as_form, level + 1));
                 }
-                let mut first = true;
                 for f in &st.fields {
-                    if !first {
-                        resp.push_str(", ");
-                        first=false;
+                    if f.name.is_empty() {
+                        resp.push_str(&dump_struct_int(structs, &f.abi_type, &format!("{}{}.", prefix, name), as_form, level + 1));
+                    } else {
+                        resp.push_str(&dump_struct_int(structs, &f.abi_type, &format!("{}{}.{}.", prefix, name, &f.name), as_form, level + 1));
                     }
-                    resp.push_str(&dump_struct_int(structs,&f.name,level+1));
-
                 }
 
-                resp.push_str("}");
+                resp.push_str("</ul>");
                 resp
-            },
-            None => name.to_owned()
+            }
+            None => {
+                // base types
+                match name {
+                    "name" => {
+                        format!("<li>{}{} <input name='{}{}' type='text' maxlength='12' size='14' required/></li>", prefix, name, prefix, name)
+                    }
+                    "string" => {
+                        format!("<li>{}{} <input name='{}{}' type='text' ' required/></li>", prefix, name, prefix, name)
+                    }  "public_key" => {
+                        format!("<li>{}{} <input name='{}{}' type='text' ' required/></li>", prefix, name, prefix, name)
+                    }
+                    "int64" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required/></li>", prefix, name, prefix, name)
+                    }
+                    "int64?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number'/></li>", prefix, name, prefix)
+                    }
+                    "int32" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required/></li>", prefix, name, prefix, name)
+                    }
+                    "int32?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number'/></li>", prefix, name, prefix)
+                    }
+                    "int16" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required/></li>", prefix, name, prefix, name)
+                    }
+                    "int16?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number'/></li>", prefix, name, prefix)
+                    }
+                    "uint64" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required min='0' /></li>", prefix, name, prefix, name)
+                    }
+                    "uint64?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number' min='0' /></li>", prefix, name, prefix)
+                    }
+                    "uint32" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required min='0' /></li>", prefix, name, prefix, name)
+                    }
+                    "uint32?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number' min='0' /></li>", prefix, name, prefix)
+                    }
+                    "uint16" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required min='0' /></li>", prefix, name, prefix, name)
+                    }
+                    "uint16?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number' min='0' /></li>", prefix, name, prefix)
+                    }
+                    "varuint32" => {
+                        format!("<li>{}{} <input name='{}{}' type='number' required min='0' /></li>", prefix, name, prefix, name)
+                    }
+                    "varuint32?" => {
+                        format!("<li>{}{} <input name='{}int64' type='number' min='0' /></li>", prefix, name, prefix)
+                    }
+                    "asset" => {
+                        format!("<li>{}{}.symbol <input name='{}{}.symbol' type='text' maxlength='12' size='14'required /> amount <input name='{}{}.amount' required type='number'/></li>",
+                                prefix, name, prefix, name,
+                                prefix, name)
+                    }
+                    "symbol" => {
+                        format!("<li>{}{} <input name='{}{}' type='text' maxlength='12' size='14'required /></li>",
+                                prefix, name, prefix, name)
+                    }
+                    "checksum256" => {
+                        format!("<li>{}{} <input name='{}{}' type='text' minlength='256' maxlength='256' size='256' required /></li>",
+                                prefix, name, prefix, name)
+                    }
+                    "producer_schedule?" => {
+                        format!("<li>{}{} <input name='{}{}' type='text'  /> ..TBD..</li>",
+                                prefix, name, prefix, name)
+                    }
+                    _ => {
+                        format!("<li><b>UNKNOWN</b>? {} __{}__</li>", prefix, name)
+                    }
+                }
+            }
         }
     }
 }
 
 pub async fn action_detail(params: web::Path<(String, String)>) -> impl Responder {
-    let host = "https://api.testnet.eos.io";
+    let host ="http://tempest.local:8888";
     let acc = String::from(params.0.as_str());
     let action_name = String::from(params.1.as_str());
     match EOSRPC::non_blocking(String::from(host)).await {
@@ -155,39 +246,35 @@ pub async fn action_detail(params: web::Path<(String, String)>) -> impl Responde
                 Ok(gabi) => {
                     match gabi.abi {
                         Some(abi) => {
-                           match abi.actions.iter().find(|p| p.name == action_name ) {
-                               Some(act) => {
-                                   let mut resp: String = "<a href='/'>Home</a><br/>".to_owned() ;
-                                   resp.push_str(&format!("Account <a href='/account/{}'>{}</a>",acc,acc));
-                                   resp.push_str(&format!("<h1>Action {}</h1>",action_name));
-                                   if ! act.ricardian_contract.is_empty()  {
-                                       resp.push_str(&format!("<h2>Contract</h2><pre>{}</pre>", &act.ricardian_contract));
-                                   }
-                                   resp.push_str(&format!("<h2>Parameters for:{}</h2>",act.abi_type));
-                                   let mut abi_map:HashMap<String,AbiStruct> = HashMap::with_capacity(abi.structs.len());
-                                   for f in abi.structs {
-                                       abi_map.insert(f.name.clone(), f);
-                                   };
+                            match abi.actions.iter().find(|p| p.name == action_name) {
+                                Some(act) => {
+                                    let mut resp: String = "<a href='/'>Home</a><br/>".to_owned();
+                                    resp.push_str(&format!("Account <a href='/account/{}'>{}</a>", acc, acc));
+                                    resp.push_str(&format!("<h1>Action {}</h1>", action_name));
+                                    if !act.ricardian_contract.is_empty() {
+                                        resp.push_str(&format!("<h2>Contract</h2><pre>{}</pre>", &act.ricardian_contract));
+                                    }
+                                    resp.push_str(&format!("<h2>Parameters for:{}</h2>", act.abi_type));
+                                    let mut abi_map: HashMap<String, AbiStruct> = HashMap::with_capacity(abi.structs.len());
+                                    for f in abi.structs {
+                                        abi_map.insert(f.name.clone(), f);
+                                    };
 
-                                   match abi_map.get(&act.abi_type[0..]) {
-                                       Some(struct_def) => {
-                                           resp.push_str(&format!("Base:{}<ul>", struct_def.base));
-                                           struct_def.fields.iter().for_each(|f| {
-                                               resp.push_str(&format!("<li>{}/{}</li>",f.name, dump_struct(&abi_map,&f.abi_type)));
-                                           });
-                                           resp.push_str("</ul>");
+                                    if abi_map.contains_key(&act.abi_type) {
+                                        resp.push_str("<form method='POST'>");
+                                        resp.push_str(&dump_struct(&abi_map, &act.abi_type, true));
+                                        resp.push_str("<input type='submit' />");
+                                        resp.push_str("</form>");
+                                    } else {
+                                        resp.push_str("<p>Not Found in ABI</p>")
+                                    }
 
-                                       },
-                                       None => resp.push_str("<p>Not Found in ABI</p>")
-                                   }
-
-                                   HttpResponse::Ok().content_type("text/html").body(resp)
-                               },
-                               None => HttpResponse::NotFound().body(format!("Action {} Not Found on Account {}",action_name,acc)),
-                           }
-                        },
-                        None=> HttpResponse::NotFound().body(acc),
-
+                                    HttpResponse::Ok().content_type("text/html").body(resp)
+                                }
+                                None => HttpResponse::NotFound().body(format!("Action {} Not Found on Account {}", action_name, acc)),
+                            }
+                        }
+                        None => HttpResponse::NotFound().body(acc),
                     }
                 }
                 Err(_e) => HttpResponse::NotFound().body(acc),
@@ -207,7 +294,7 @@ pub async fn table_rows(
     params: web::Path<(String, String, String)>,
     qry: Query<TableRows>,
 ) -> impl Responder {
-    let host = "https://api.testnet.eos.io";
+    let host ="http://tempest.local:8888";
     let acc = String::from(params.0.as_str());
     let scope_name = String::from(params.1.as_str());
     let table_name = String::from(params.2.as_str());
@@ -297,6 +384,7 @@ pub async fn table_rows(
                             )
                         }
                         Err(e) => {
+                            println!("{:?}",e);
                             HttpResponse::InternalServerError().body(String::from(e.description()))
                         }
                     }
@@ -304,6 +392,9 @@ pub async fn table_rows(
                 Err(_e) => HttpResponse::NotFound().body(acc),
             }
         }
-        Err(e) => HttpResponse::InternalServerError().body(String::from(e.description())),
+        Err(e) => {
+            println!("{:?}",e);
+            HttpResponse::InternalServerError().body(String::from(e.description()))
+        },
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,21 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(handlers::index))
+            .service(
+                web::resource("/account/{account}/action/{action_name}")
+                    .route(web::get().to(handlers::action_detail))
+                    .route(web::post().to(handlers::action_detail_post)),
+            )
+            /*
             .route(
                 "/account/{account}/action/{action_name}",
                 web::get().to(handlers::action_detail),
+            )  .route(
+                "/account/{account}/action/{action_name}",
+                web::post().to(handlers::action_detail_post),
             )
+
+             */
             .route(
                 "/account/{account}/table/{table_name}",
                 web::get().to(handlers::table_detail),
